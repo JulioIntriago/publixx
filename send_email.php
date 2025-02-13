87c028f787b53f1baa7294867123fb4f5586af95
julio <juliointriago96@hotmail.com>
2025-02-13 15:31:29 -0500

    cambios

---
+++ send_email.php
@@ -0,0 +1,25 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST["name"]);
+    $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    $message = htmlspecialchars($_POST["message"]);
+
+    $to = "elevateclaimadjusters@gmail.com"; // Tu correo
+    $subject = "New Contact Form Message from $name";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    $body = "Name: $name\n";
+    $body .= "Email: $email\n\n";
+    $body .= "Message:\n$message\n";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "success";
+    } else {
+        echo "error";
+    }
+} else {
+    echo "Invalid request.";
+}
+?>
